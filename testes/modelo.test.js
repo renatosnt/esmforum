d594dc314b28730cf7a7a98318e81debd58e440b
Renato <renatossnt@gmail.com>
2022-11-27 10:49:31 -0300

    100% de cobertura de testes

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -1,25 +1,48 @@
-const bd = require('../bd/bd_utils.js');
-const modelo = require('../modelo.js');
+const bd = require("../bd/bd_utils.js");
+const modelo = require("../modelo.js");
 
 beforeEach(() => {
-  bd.reconfig('./bd/esmforum-teste.db');
+  bd.reconfig("./bd/esmforum-teste.db");
   // limpa dados de todas as tabelas
-  bd.exec('delete from perguntas', []);
-  bd.exec('delete from respostas', []);
+  bd.exec("delete from perguntas", []);
+  bd.exec("delete from respostas", []);
 });
 
-test('Testando banco de dados vazio', () => {
+test("Testando banco de dados vazio", () => {
   expect(modelo.listar_perguntas().length).toBe(0);
 });
 
-test('Testando cadastro de três perguntas', () => {
-  modelo.cadastrar_pergunta('1 + 1 = ?');
-  modelo.cadastrar_pergunta('2 + 2 = ?');
-  modelo.cadastrar_pergunta('3 + 3 = ?');
-  const perguntas = modelo.listar_perguntas(); 
+test("Testando cadastro de três perguntas", () => {
+  modelo.cadastrar_pergunta("1 + 1 = ?");
+  modelo.cadastrar_pergunta("2 + 2 = ?");
+  modelo.cadastrar_pergunta("3 + 3 = ?");
+  const perguntas = modelo.listar_perguntas();
   expect(perguntas.length).toBe(3);
-  expect(perguntas[0].texto).toBe('1 + 1 = ?');
-  expect(perguntas[1].texto).toBe('2 + 2 = ?');
+  expect(perguntas[0].texto).toBe("1 + 1 = ?");
+  expect(perguntas[1].texto).toBe("2 + 2 = ?");
   expect(perguntas[2].num_respostas).toBe(0);
-  expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
+  expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta - 1);
+});
+
+test("Testando cadastro de três respostas", () => {
+  modelo.cadastrar_resposta(0, "2");
+  modelo.cadastrar_resposta(1, "4");
+  modelo.cadastrar_resposta(2, "6");
+  expect(modelo.get_respostas(0)[0].texto).toBe("2");
+  expect(modelo.get_respostas(1)[0].texto).toBe("4");
+  expect(modelo.get_respostas(2)[0].texto).toBe("6");
+});
+
+test("Testando get perguntas para 3 perguntas", () => {
+  modelo.cadastrar_pergunta("1 + 1 = ?");
+  modelo.cadastrar_pergunta("2 + 2 = ?");
+  modelo.cadastrar_pergunta("3 + 3 = ?");
+  const perguntas = modelo.listar_perguntas();
+  id = [];
+  perguntas.forEach((pergunta) => {
+    id.push(pergunta.id_pergunta);
+  });
+  expect(modelo.get_pergunta(id[0]).texto).toBe("1 + 1 = ?");
+  expect(modelo.get_pergunta(id[1]).texto).toBe("2 + 2 = ?");
+  expect(modelo.get_pergunta(id[2]).texto).toBe("3 + 3 = ?");
 });
